cd86eecd7803de1e13c2b3a0c1144a0cf2ae0ecb
Majdi Jebril <majdijebril@Ashrafs-MacBook-Pro.local>
2021-01-21 21:44:10 +1100

    fix

--- src/pages/TAManagments.vue
+++ src/pages/TAManagments.vue
@@ -42,15 +42,44 @@
       </div>
       <!-- maltiple
           use-chips
-          stack-label -->
+      stack-label-->
       <div class="w-1/4 px-4">
         <q-select
           outlined
           dense
           v-model="subjects"
           :options="getProgramSubjects"
+          multiple
           label="Subjects"
         />
+      </div>
+      <button class="bg-blue-300 p-2 rounded px-4 text-white mr-4" @click="search">Search</button>
+
+      <button class="bg-blue-300 p-2 rounded px-4 text-white" @click="addTa">Add</button>
+    </div>
+
+    <div class="p-4">
+      <div class="flex justify-between text-xl mb-4">
+        <div class="w-1/5">Name</div>
+        <div class="w-1/5">Email</div>
+        <div class="w-1/5">Country</div>
+        <div class="w-1/5">Program</div>
+        <div class="w-1/5">Subject</div>
+      </div>
+      <div v-for="(item) in items" :key="item.id">
+        <div
+          class="flex justify-between cursor-pointer border-2 mb-2 border-light-blue-500"
+          @click="chooseTa=item.id"
+          :class="chooseTa==item.id?'bg-gray-200':''"
+        >
+          <div class="w-1/5">{{item.name}}</div>
+          <div class="w-1/5">{{item.email}}</div>
+          <div class="w-1/5">{{item.country}}</div>
+          <div class="w-1/5">{{item.program}}</div>
+          <div class="w-1/5">
+            <div class v-for="(sub,index) in item.subject" :key="index">{{sub.label}}</div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -63,32 +92,101 @@ export default {
   },
   data() {
     return {
+      chooseTa: "",
+      items: [
+        {
+          id: 1,
+          name: "ashraf",
+          email: "ashraf@abwaab.me",
+          country: "",
+          program: "",
+          subject: []
+        },
+        {
+          id: 2,
+          name: "abdullah",
+          email: "abudllah@abwaab.me",
+          country: "",
+          program: "",
+          subject: []
+        },
+        {
+          id: 3,
+          name: "hamdi",
+          email: "hamdi@abwaab.me",
+          country: "",
+          program: "",
+          subject: []
+        },
+        {
+          id: 4,
+          name: "hussien",
+          email: "hussien@abwaab.me",
+          country: "",
+          program: "",
+          subject: []
+        },
+        {
+          id: 5,
+          name: "wajd",
+          email: "wajd@abwaab.me",
+          country: "",
+          program: "",
+          subject: []
+        }
+      ],
       program: null,
       Countries: null,
-      subjects: null,
+      subjects: [],
       websiteCountries: [
         {
           id: 1,
           value: "JO",
           label: "Jordan",
-          icon: require("../assets/flags/Jordan_flag.png"),
+          icon: require("../assets/flags/Jordan_flag.png")
         },
         {
           id: 2,
           value: "PS",
           label: "Palestine",
-          icon: require("../assets/flags/Palestine_flag.png"),
+          icon: require("../assets/flags/Palestine_flag.png")
         },
         {
           id: 3,
           value: "EG",
           label: "Egypt",
-          icon: require("../assets/flags/Egypt_flag.png"),
-        },
-      ],
+          icon: require("../assets/flags/Egypt_flag.png")
+        }
+      ]
     };
   },
   methods: {
+    search() {
+      var searchResult = [];
+      for (let index = 0; index < this.items.length; index++) {
+        console.log("asfa", this.items[index].program, this.program.label);
+
+        if (this.items[index].program === this.program.label) {
+          console.log("yesss");
+          searchResult.push(this.items[index]);
+        }
+      }
+      this.items = searchResult;
+      console.log("ite", this.items);
+    },
+    addTa() {
+      console.log("tsat");
+      for (let index = 0; index < this.items.length; index++) {
+        if (this.items[index].id == this.chooseTa) {
+          this.items[index].country = this.Countries.label;
+          this.items[index].program = this.program.label;
+
+          this.items[index].subject = this.subjects;
+          console.log("this.items[index].subject", this.items[index].subject);
+        }
+      }
+    },
+
     ...mapActions(["getPrograms", "getProgramSubject"]),
     getSubjects() {
       this.getProgramSubject(this.program.value);
@@ -96,19 +194,19 @@ export default {
     clear() {
       this.program = null;
       this.subjects = null;
-    },
+    }
   },
   computed: {
     ...mapGetters(["programs", "ProgramSubjects"]),
     getProgramsByCountry() {
       if (this.Countries) {
         let programs = [];
-        this.programs.filter((program) => {
+        this.programs.filter(program => {
           if (program.country == this.Countries.value) {
             programs.push({
               id: program.id,
               label: program.title_ar,
-              value: program.id,
+              value: program.id
             });
           }
         });
@@ -118,16 +216,16 @@ export default {
     getProgramSubjects() {
       this.subjects = null;
       let subjects = [];
-      this.ProgramSubjects.filter((subject) => {
+      this.ProgramSubjects.filter(subject => {
         subjects.push({
           id: subject.id,
           label: subject.title_ar,
-          value: subject.id,
+          value: subject.id
         });
       });
       return subjects;
-    },
-  },
+    }
+  }
 };
 </script>
 <style scoped>
